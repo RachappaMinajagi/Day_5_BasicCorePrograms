2c40003153dbe5d77210581c765f948e6ea43191
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-17 22:13:53 +0530

    Power of 2 number

--- src/PowerOf2.java
+++ src/PowerOf2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PowerOf2 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of n");
 
