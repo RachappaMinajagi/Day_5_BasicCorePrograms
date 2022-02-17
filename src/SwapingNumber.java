e7956c7d3b268e446100df7648c40c37c72f790f
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-17 22:15:53 +0530

    Swapping of two Number

--- src/SwapingNumber.java
+++ src/SwapingNumber.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class SwapingNumber {
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
